0e88d055601d8be7170290d1001c7f29614cd6db
tizianofassina <tifas98@gmail.com>
2025-03-02 15:54:54 +0100

    data cleaned

--- data_preproc.py
+++ data_preproc.py
@@ -1,4 +1,31 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
-with open("")
+
+with open("data_meuse_corsica/max-meuse.txt") as file:
+    data = file
+    meuse_data = []
+
+    for element in data:
+        elements = element.split()
+        if element!="\n":
+            meuse_data.append(float(elements[0]))
+
+
+df = pd.read_csv("data_meuse_corsica/pluviometry-corsica.csv")
+
+with open("data_meuse_corsica/pluviometry-corsica.csv") as file:
+    data = file
+    corsica_data = []
+    header = next(data)
+
+    for element in data:
+        elements = element.split(";")
+        if element!="\n":
+            number = elements[2][:-1]
+            number = number.replace(',','.')
+            corsica_data.append(float(number))
+
+np.save( "data_meuse_corsica/numpy_meuse.npy", np.array(meuse_data))
+
+np.save( "data_meuse_corsica/numpy_corsica.npy", np.array(corsica_data))
