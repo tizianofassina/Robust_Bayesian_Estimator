ebec5c120ed4d71dce596050ac8c42500296d631
tizianofassina <tifas98@gmail.com>
2025-04-03 17:43:28 +0200

    corsica done

--- optimization_corsica.py
+++ optimization_corsica.py
@@ -48,23 +48,13 @@ def possible_values(n, length_theta, number_candidates, quantiles, alphas, std=4
 
             if all(p >= 0):
                 possible_values.append((theta, p))
-
             else:
                 pass
         else:
             pass
 
-    if os.path.exists("possible_values_corsica.pkl"):
-        with open("possible_values_corsica.pkl", "rb") as f:
-            existing_values = pickle.load(f)
-
-        updated_values = existing_values + possible_values
-
-        with open("possible_values_corsica.pkl", "wb") as f:
-            pickle.dump(updated_values, f)
-    else:
-        with open("possible_values_corsica.pkl", "wb") as f:
-            pickle.dump(possible_values, f)
+    with open("possible_values_corsica.pkl", "wb") as f:
+        pickle.dump(possible_values, f)
 
     print(f"Added {number_candidates} new possible candidates")
 
@@ -80,6 +70,7 @@ def best_values_for_sup(x, data, possible_values, number_best):
         value = single_evaluation(x, theta, data, p)
 
         if not np.isnan(value) and value != 0:
+
             values.append(value)
             valid_possible_values.append(element)
 
@@ -87,7 +78,6 @@ def best_values_for_sup(x, data, possible_values, number_best):
 
     top_k_indices = np.argpartition(values, -number_best)[-number_best:]
     top_k_indices = top_k_indices[np.argsort(values[top_k_indices])[::-1]]
-
     with open("best_initial_points_for_sup_corsica.pkl", "wb") as f:
         pickle.dump([valid_possible_values[i] for i in top_k_indices], f)
 
@@ -152,66 +142,104 @@ constraints = [
 ]
 
 
-
-
-def optimization(function, best_values, x, data):
-
+def optimization(function, best_values, x, data, maximize=True):
     result = []
+    best_point = []
+
+    options = {
+        'verbose': 0,
+        'gtol': 1e-8,
+        'xtol': 1e-8,
+        'maxiter': 250,
+        'initial_tr_radius': 1.0,
+    }
+
     for element in best_values:
         initial_params = np.concatenate([element[0].flatten(), element[1]])
-        if np.any(likelihood(corsica_data, element[0]) != 0) :
-
-            result.append( - minimize(
+        if np.any(likelihood(corsica_data, element[0]) != 0):
+            optim = minimize(
                 function,
                 x0=initial_params,
                 args=(x, data),
                 constraints=constraints,
-                method='trust-constr'
-            ).fun)
+                method='trust-constr',
+                options = options
+            )
+            value = -optim.fun if maximize else optim.fun
+            result.append(value)
+            best_point.append(optim.x)
 
+    if not result:
+        print("No valid result finded.")
+        return None, None
 
-    return max(result)
+    i = np.argmax(result) if maximize else np.argmin(result)
+
+    return result[i], best_point[i]
+
 
 
 if __name__ == "__main__":
 
-    x_s = np.arange(40, 400, step = 10.)
-
     number_best = 100
+    possible_values(n, length_theta, 10000, quantiles, alphas)
 
 
-    possible_values(n, length_theta,100000, quantiles, alphas)
+    x_s = np.arange(1., max(corsica_data) + 100, step=10.)
 
     with open("possible_values_corsica.pkl", "rb") as f:
         possible = pickle.load(f)
 
-    supremum = []
+
     infimum = []
+    argmax = []
+    supremum = []
+    argmin = []
+
+    i = 0
+
     for x in x_s:
-        best_values_for_sup(x, corsica_data, possible, number_best)
-        with open("best_initial_points_for_sup_corsica.pkl", "rb") as f:
-            bests_max = pickle.load(f)
+        i+=1
+        while True:
 
-        supremum.append(optimization(function_for_maximization, bests_max, x, corsica_data))
+            try:
+                if len(supremum) == 0 or supremum[-1] < 1.:
+                    best_values_for_sup(x, corsica_data, possible, number_best)
+                    with open("best_initial_points_for_sup_corsica.pkl", "rb") as f:
+                        bests_max = pickle.load(f)
 
-        best_values_for_inf(x, corsica_data, possible, number_best)
+                    result_max, arg_max = optimization(function_for_maximization, bests_max, x, corsica_data, maximize=True)
+                supremum.append(float(result_max))
+                argmax.append(arg_max)
 
-        with open("best_initial_points_for_inf_corsica.pkl", "rb") as f:
-                bests_min = pickle.load(f)
-        infimum.append(optimization(function_for_minimization, bests_min, x, corsica_data))
+                best_values_for_inf(x, corsica_data, possible, number_best)
+                with open("best_initial_points_for_inf_corsica.pkl", "rb") as f:
+                    bests_min = pickle.load(f)
 
-    np.save(np.array(supremum), "Sup_corsica.npy")
-    np.save(np.array(infimum), "Inf_corscica.npy")
+                result_min, arg_min = optimization(function_for_minimization, bests_min, x, corsica_data, maximize=False)
+                infimum.append(float(result_min))
+                argmin.append(arg_min)
+                print(f"Progress {i / len(x_s) * 100:.2f}% - Point x: {x}, Sup: {result_max}, Inf: {result_min}")
+
+                break
+            except:
+                possible_values(n, length_theta, 100000, quantiles, alphas)
+                print(f"⚠️ Error in optimization at x = {x}")
+                print("⚠️ Ricomputing possible values due to error in optimization")
+                with open("possible_values_corsica.pkl", "rb") as f:
+                    possible = pickle.load(f)
 
 
 
+    print("🏁 Optimization completed.")
+    print(supremum)
+    print(infimum)
 
-
-
-
-
-
-
+    np.save("Sup_corsica.npy", np.array(supremum))
+    np.save("Inf_corsica.npy", np.array(infimum))
+    np.save("Argmax_corsica.npy", np.array(argmax))
+    np.save("Argmin_corsica.npy", np.array(argmin))
+    np.save("x_corsica.npy", np.array(x_s))
 
 
 
--- optimization_meuse.py
+++ optimization_meuse.py
@@ -68,7 +68,9 @@ def possible_values(n, length_theta, number_candidates, quantiles, alphas, std=4
         with open("possible_values_meuse.pkl", "wb") as f:
             pickle.dump(possible_values, f)
 
-    return print(f"Added {number_candidates} new possible candidates")
+    print(f"Added {number_candidates} new possible candidates")
+
+    return 0
 
 
 def best_values_for_sup(x, data, possible_values, number_best):
@@ -91,8 +93,9 @@ def best_values_for_sup(x, data, possible_values, number_best):
 
     with open("best_initial_points_for_sup_meuse.pkl", "wb") as f:
         pickle.dump([valid_possible_values[i] for i in top_k_indices], f)
+    print(f"Created the file with {number_best} initial points")
 
-    return print(f"Created the file with {number_best} initial points")
+    return 0
 
 def best_values_for_inf(x, data, possible_values, number_best):
     values = []
@@ -115,8 +118,9 @@ def best_values_for_inf(x, data, possible_values, number_best):
 
     with open("best_initial_points_for_inf_meuse.pkl", "wb") as f:
         pickle.dump([valid_possible_values[i] for i in top_k_indices], f)
+    print(f"Created the file with {number_best} initial points")
 
-    return print(f"Created the file with {number_best} initial points")
+    return 0
 
 
 def function_for_maximization(theta_p, x , data):
@@ -151,54 +155,103 @@ constraints = [
 
 
 
-def optimization(function, best_values, x, data):
-
+def optimization(function, best_values, x, data, maximize=True):
     result = []
+    best_point = []
+
+    options = {
+        'verbose': 0,
+        'gtol': 1e-8,
+        'xtol': 1e-8,
+        'maxiter': 250,
+        'initial_tr_radius': 1.0,
+    }
+
     for element in best_values:
         initial_params = np.concatenate([element[0].flatten(), element[1]])
-        if np.any(likelihood(meuse_data, element[0]) != 0) :
-
-            result.append( - minimize(
+        if np.any(likelihood(meuse_data, element[0]) != 0):
+            optim = minimize(
                 function,
                 x0=initial_params,
                 args=(x, data),
                 constraints=constraints,
-                method='trust-constr'
-            ).fun)
+                method='trust-constr',
+                options = options
+            )
+            value = -optim.fun if maximize else optim.fun
+            result.append(value)
+            best_point.append(optim.x)
 
+    if not result:
+        print("No valid result finded.")
+        return None, None
 
-    return max(result)
+    i = np.argmax(result) if maximize else np.argmin(result)
+
+    return result[i], best_point[i]
 
 
 if __name__ == "__main__":
 
-    x_s = np.arange(450, 3300, step= 40.)
-
     number_best = 100
+    possible_values(n, length_theta, 10000, quantiles, alphas)
 
-    possible_values(n, length_theta, 100000, quantiles, alphas)
+
+    x_s = np.arange(1., max(meuse_data) + 100, step=10.)
 
     with open("possible_values_meuse.pkl", "rb") as f:
         possible = pickle.load(f)
 
-    supremum = []
+
     infimum = []
+    argmax = []
+    supremum = []
+    argmin = []
+
+    i = 0
+
     for x in x_s:
-        best_values_for_sup(x, meuse_data, possible, number_best)
-        with open("best_initial_points_for_sup_meuse.pkl", "rb") as f:
-            bests_max = pickle.load(f)
+        i+=1
+        while True:
 
-        supremum.append(optimization(function_for_maximization, bests_max, x, meuse_data))
+            try:
+                if len(supremum) == 0 or supremum[-1] < 1.:
+                    best_values_for_sup(x, meuse_data, possible, number_best)
+                    with open("best_initial_points_for_sup_meuse.pkl", "rb") as f:
+                        bests_max = pickle.load(f)
 
-        best_values_for_inf(x, meuse_data, possible, number_best)
+                    result_max, arg_max = optimization(function_for_maximization, bests_max, x, meuse_data, maximize=True)
+                supremum.append(float(result_max))
+                argmax.append(arg_max)
 
-        with open("best_initial_points_for_inf_meuse.pkl", "rb") as f:
-            bests_min = pickle.load(f)
-        infimum.append(optimization(function_for_minimization, bests_min, x, meuse_data))
+                best_values_for_inf(x, meuse_data, possible, number_best)
+                with open("best_initial_points_for_inf_meuse.pkl", "rb") as f:
+                    bests_min = pickle.load(f)
 
-    np.save(np.array(supremum), "Sup_meuse.npy")
-    np.save(np.array(infimum), "Inf_meuse.npy")
+                result_min, arg_min = optimization(function_for_minimization, bests_min, x, meuse_data, maximize=False)
+                infimum.append(float(result_min))
+                argmin.append(arg_min)
+                print(f"Progress {i / len(x_s) * 100:.2f}% - Point x: {x}, Sup: {result_max}, Inf: {result_min}")
 
+                break
+            except:
+                possible_values(n, length_theta, 100000, quantiles, alphas)
+                print(f"⚠️ Error in optimization at x = {x}")
+                print("⚠️ Ricomputing possible values due to error in optimization")
+                with open("possible_values_meuse.pkl", "rb") as f:
+                    possible = pickle.load(f)
+
+
+
+    print("🏁 Optimization completed.")
+    print(supremum)
+    print(infimum)
+
+    np.save("Sup_meuse.npy", np.array(supremum))
+    np.save("Inf_meuse.npy", np.array(infimum))
+    np.save("Argmax_meuse.npy", np.array(argmax))
+    np.save("Argmin_meuse.npy", np.array(argmin))
+    np.save("x_meuse.npy", np.array(x_s))
 
 
 
